2ba7e451043968d39dc35572c552a919a86c67e0
Emmanuel Gautier <git@emmanuelgautier.fr>
2023-04-12 19:15:55 +0200

    feat: add audience support

--- generator/generator.go
+++ generator/generator.go
@@ -13,9 +13,10 @@ type Generator struct {
 	authenticators map[string]Authenticator
 	PrefixId       string
 
-	ServerUrls     *[]string
-	JwksUris       *map[string]string
-	AllowedIssuers *map[string]string
+	ServerUrls       []string
+	JwksUris         map[string]string
+	AllowedIssuers   map[string]string
+	AllowedAudiences map[string]string
 }
 
 type AuthenticatorType string
@@ -36,7 +37,7 @@ func (g *Generator) computeId(operationId string) string {
 }
 
 func (g *Generator) createRule(verb string, path string, o *openapi3.Operation) (*rule.Rule, error) {
-	match, matchRuleErr := createMatchRule(*g.ServerUrls, verb, path)
+	match, matchRuleErr := createMatchRule(g.ServerUrls, verb, path)
 	if matchRuleErr != nil {
 		return nil, matchRuleErr
 	}
@@ -88,13 +89,14 @@ func (g *Generator) createRule(verb string, path string, o *openapi3.Operation) 
 	return &rule, nil
 }
 
-func NewGenerator(prefixId string, jwksUris *map[string]string, allowedIssuers *map[string]string, serverUrls *[]string) *Generator {
+func NewGenerator(prefixId string, jwksUris map[string]string, allowedIssuers map[string]string, allowedAudiences map[string]string, serverUrls []string) *Generator {
 	return &Generator{
 		PrefixId: prefixId,
 
-		JwksUris:       jwksUris,
-		AllowedIssuers: allowedIssuers,
-		ServerUrls:     serverUrls,
+		JwksUris:         jwksUris,
+		AllowedIssuers:   allowedIssuers,
+		AllowedAudiences: allowedAudiences,
+		ServerUrls:       serverUrls,
 	}
 }
 
@@ -107,7 +109,7 @@ func (g *Generator) LoadOpenAPI3Doc(ctx context.Context, d *openapi3.T) error {
 
 	if g.ServerUrls == nil {
 		for _, s := range g.doc.Servers {
-			*g.ServerUrls = append(*g.ServerUrls, s.URL)
+			g.ServerUrls = append(g.ServerUrls, s.URL)
 		}
 	}
 
@@ -121,21 +123,30 @@ func (g *Generator) LoadOpenAPI3Doc(ctx context.Context, d *openapi3.T) error {
 }
 
 func (g *Generator) getSSJwksUri(ssn string) (string, error) {
-	jwksUri, jwksUriExists := (*g.JwksUris)[ssn]
+	jwksUri, jwksUriExists := (g.JwksUris)[ssn]
 	if !jwksUriExists {
-		return "", errors.New("no jwksUris for a given security scheme")
+		return "", errors.New("no jwksUris found for a given security scheme")
 	}
 
 	return jwksUri, nil
 }
 
 func (g *Generator) getSSIssuer(ssn string) (string, error) {
-	issuer, issuerExists := (*g.AllowedIssuers)[ssn]
+	issuer, issuerExists := (g.AllowedIssuers)[ssn]
 	if !issuerExists {
-		return "", errors.New("no issuer for a given security scheme")
+		return "", errors.New("no issuer found for a given security scheme")
 	}
 
 	return issuer, nil
+}
+
+func (g *Generator) getSSAudience(ssn string) (string, error) {
+	audience, audienceExist := (g.AllowedAudiences)[ssn]
+	if !audienceExist || audience == "" {
+		return "", errors.New("no audience found for a given security scheme")
+	}
+
+	return audience, nil
 }
 
 func (g *Generator) createAuthenticators(doc *openapi3.T) (map[string]Authenticator, error) {
@@ -146,7 +157,9 @@ func (g *Generator) createAuthenticators(doc *openapi3.T) (map[string]Authentica
 		sstype := ss.Value.Type
 		switch sstype {
 		case string(AuthenticatorTypeOpenIdConnect):
-			authenticators[ssn], err = NewAuthenticatorOpenIdConnect(ss)
+			audience, _ := g.getSSAudience(ssn)
+
+			authenticators[ssn], err = NewAuthenticatorOpenIdConnect(ss, audience)
 		case string(AuthenticatorTypeOAuth2):
 			jwksUri, jwksUriErr := g.getSSJwksUri(ssn)
 			if jwksUriErr != nil {
@@ -158,7 +171,9 @@ func (g *Generator) createAuthenticators(doc *openapi3.T) (map[string]Authentica
 				return nil, issuerErr
 			}
 
-			authenticators[ssn], err = NewAuthenticatorOAuth2(ss, jwksUri, issuer)
+			audience, _ := g.getSSAudience(ssn)
+
+			authenticators[ssn], err = NewAuthenticatorOAuth2(ss, jwksUri, issuer, audience)
 		case string(AuthenticatorTypeHttp):
 			if ss.Value.Scheme != "bearer" {
 				return nil, errors.New("http security scheme must be bearer")
@@ -174,7 +189,12 @@ func (g *Generator) createAuthenticators(doc *openapi3.T) (map[string]Authentica
 				return nil, issuerErr
 			}
 
-			authenticators[ssn], err = NewAuthenticatorHttpBearer(ss, jwksUri, issuer)
+			audience, audienceErr := g.getSSAudience(ssn)
+			if audienceErr != nil {
+				return nil, audienceErr
+			}
+
+			authenticators[ssn], err = NewAuthenticatorHttpBearer(ss, jwksUri, issuer, audience)
 
 		default:
 			return nil, errors.New("unknown security scheme")
