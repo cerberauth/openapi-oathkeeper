073fcb009d9cb1cddb790f9434cc1e3e3555ea4f
Emmanuel Gautier <git@emmanuelgautier.fr>
2023-07-26 22:08:44 +0200

    fix: add back default mutators, errors and upstream values

--- config/default.go
+++ config/default.go
@@ -1,3 +1,22 @@
 package config
 
-var defaultConfig = map[string]interface{}{}
+import "github.com/ory/oathkeeper/rule"
+
+var defaultConfig = map[string]interface{}{
+	"authorizers": []rule.Handler{
+		rule.Handler{
+			Handler: "allow",
+		},
+	},
+	"mutators": []rule.Handler{
+		rule.Handler{
+			Handler: "noop",
+		},
+	},
+	"errors": []rule.ErrorHandler{
+		rule.ErrorHandler{
+			Handler: "json",
+		},
+	},
+	"upstream": rule.Upstream{},
+}
